bb6416cf5430cbda9a063e10fe512016e7429622
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-05-20 16:56:02 +0800

    移除主页场景代码

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import { configure } from 'mobx';
 import { observer, Provider } from 'mobx-react';
 import React from 'react';
-import ReactResizeDetector from 'react-resize-detector';
 import Panel from '../components/Panel';
 import PilotScene from '../components/Scene';
 import { stores } from '../stores';
-import styles from './index.less';
 
 configure({
   enforceActions: true
@@ -13,13 +11,6 @@ configure({
 
 @observer
 export default class IndexPage extends React.Component<{}> {
-
-  containerRef = React.createRef<HTMLDivElement>();
-
-  onResize = (width: number, height: number) => {
-    console.log(width, height);
-  }
-
   render() {
     return (
       <Provider { ...stores }>
